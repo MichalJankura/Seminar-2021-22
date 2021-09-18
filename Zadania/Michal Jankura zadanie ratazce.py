72cee029fc507abd6d6236e3bcc8140b9a11e017
Michal Jankura <72251818+MichalJankura@users.noreply.github.com>
2021-09-18 15:04:04 +0200

    Update Michal Jankura zadanie ratazce.py

--- Zadania/Michal Jankura zadanie ratazce.py
+++ Zadania/Michal Jankura zadanie ratazce.py
@@ -4,20 +4,24 @@ meno = str("Eva")
 m = len(meno)
 
 #úloha 1 ###100%###
+
 # for i in range (m):
 #     print(meno,end=" ")
 
 #úloha 2 ###100%###
+
 # for i in range (m):
 #     print(meno)
 
 #úloha 3 ###100%###
+
 # m = m+6
 # print(m*"*")
 # print(f"*  {meno}  *")
 # print(m*"*")
 
 #úloha 4 ###100%###
+
 # for i in range(m):
 #     print(meno[i]*(i+1))
 
@@ -25,9 +29,15 @@ m = len(meno)
 # for i in range(m):
 #     print(" "*(m-i),meno[i]*(i+1))
 
-# #úloha 6 ###0%###
+# #úloha 6 ###100%###
+
+# for i in range(m):
+#     print(meno[:i+1])
 
 # #úloha 7 ###0%###
+
+# for i in range(m):
+#     print(" "*(m-i),meno[:i+1])
 
 # #úloha 8 ###100%###
 # print(meno[::-1])
