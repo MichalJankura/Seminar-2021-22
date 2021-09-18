9081bf50143685759e01890b0d394cbd472f1997
Michal Jankura <72251818+MichalJankura@users.noreply.github.com>
2021-09-18 14:09:40 +0200

    Add files via upload

---
+++ Zadania/Michal Jankura zadanie ratazce.py
@@ -0,0 +1,34 @@
+#meno = str(input("Zadaj meno : "))
+
+meno = str("Eva")
+m = len(meno)
+
+#úloha 1 ###100%###
+# for i in range (m):
+#     print(meno,end=" ")
+
+#úloha 2 ###100%###
+# for i in range (m):
+#     print(meno)
+
+#úloha 3 ###100%###
+# m = m+6
+# print(m*"*")
+# print(f"*  {meno}  *")
+# print(m*"*")
+
+#úloha 4 ###100%###
+# for i in range(m):
+#     print(meno[i]*(i+1))
+
+#úloha 5 ###100%###
+# for i in range(m):
+#     print(" "*(m-i),meno[i]*(i+1))
+
+# #úloha 6 ###0%###
+
+# #úloha 7 ###0%###
+
+# #úloha 8 ###100%###
+# print(meno[::-1])
+
