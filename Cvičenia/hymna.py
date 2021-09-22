2e90d64e510c8e200edf17a71de1815aba3365e2
Michal Jankura <72251818+MichalJankura@users.noreply.github.com>
2021-09-22 15:56:04 +0200

    Update hymna.py

--- Cvičenia/hymna.py
+++ Cvičenia/hymna.py
@@ -6,11 +6,11 @@ print(d)#vypíš
 for i in range(0,d,n):
     print(hymna[i:i+n])
 
-###
-def rozsekaj(text,sirka):
-    i = 0
-    while i <= len(text):
+###riešenie
+def rozsekaj(text,sirka):#požadované parametre sú text teda hymna a pocet písmen po kolkých sa bude zalamovať text
+    i = 0 #vytvoríme premennú i z hodnotou 0
+    while i <= len(text):#kým bude i menšie alebo rovné dĺžke textu sprav:...
         i += sirka
-        text = text[:i] + "\n" + text[i:]
-        i += 1
-    return(text)
+        text = text[:i] + "\n" + text[i:]#text od zaciatku [:i] (pridaj) + "\n" + text do konca[i:]
+        i += 1 
+    return(text)#vráť upravený text
