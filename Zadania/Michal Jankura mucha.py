50f023a83c84d5eb568f3c8325571849a0a2ea4b
Michal Jankura <72251818+MichalJankura@users.noreply.github.com>
2021-09-24 10:04:25 +0200

    Update Michal Jankura mucha.py

--- Zadania/Michal Jankura mucha.py
+++ Zadania/Michal Jankura mucha.py
@@ -3,3 +3,28 @@ zmena = str(input("Zadaj písmeno , pre zmenu všetkých samohlások : "))
 text = "sedí mucha na stene a spí "
 display = re.sub('[aeiouóíúéý]', zmena, text)
 print(display)
+
+###RIEŠENIE PODĽA ZADANIA###
+# samohlasky = "aeiouóíúéý"
+# riekanka = "sedí mucha na stene sedí a spí"
+# print(riekanka)
+# def zmena(text,n):
+#     for znak in samohlasky:
+#         text = text.replace(znak,n)
+#     print(text)
+# n = input("Vlož samohlásku : ")
+# zmena(riekanka,n)
+
+###RIEŠENIE PODĽA ZADANIA###
+# def zmena(text,samohlaska):
+#     zmena = ""
+#     for znak in text:
+#         if znak in samohlasky:
+#             zmena += samohlaska
+#         else:
+#             zmena += znak
+#     return zmena
+#
+# samohlasky = "aeiouóíúéý"
+# riekanka = "sedí mucha na stene sedí a spí"
+# print(riekanka)
