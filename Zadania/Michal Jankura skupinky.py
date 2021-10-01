969b4cd17e02c3a1deae5c2a8daaaf08eaef30f7
Michal Jankura <72251818+MichalJankura@users.noreply.github.com>
2021-10-01 11:23:34 +0200

    Update Michal Jankura skupinky.py

--- Zadania/Michal Jankura skupinky.py
+++ Zadania/Michal Jankura skupinky.py
@@ -10,12 +10,20 @@ def nazov(n):
     return meno
 
 def zapis():
-    global m,n,i
-    m = int(input("Zadj pocet skupín :"))
+    global m,n,zoznam
+    zoznam = []
+    m = int(input("Zadaj pocet skupín :"))
+    n  = int(input("Zadaj n : "))
     for i in range(m):
-        n  = int(input("Zadaj n : "))
         subor = open("Michal Jankura.txt","a",encoding="UTF-8")
         subor.write(nazov(n)+"\n")
+
+        file = open("Michal Jankura.txt","r",encoding="UTF-8")
+        txt = file.read()
+        mena_skupin = txt.split()
+        zoznam.append(mena_skupin)
+    print("Pocet skupín : ",len(mena_skupin)+1)#pocita od nuly
+    print(zoznam)
 zapis()
-print(nazov(n))
+
 
