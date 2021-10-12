c91185688377a2abf9771a41f4a7ddc515028265
Michal Jankura <72251818+MichalJankura@users.noreply.github.com>
2021-10-12 14:05:15 +0200

    Update Michal Jankura výlet.py

--- Zadania/Michal Jankura výlet.py
+++ Zadania/Michal Jankura výlet.py
@@ -1,3 +1,4 @@
+import random
 file = open("studenti.txt","r",encoding="UTF-8")
 subor = file.readlines()
 chlapci = []
@@ -6,7 +7,14 @@ for i in subor:
     i = i.strip()
     if i[-1] == "a" :
         dievcata.append(i)
+        dievcata.sort()
     else:
         chlapci.append(i)
+        chlapci.sort()
+
+
+print(dievcata)
 print("Počet dievčat je",len(dievcata))
+print(chlapci)
 print("Počet chlapcov je",len(chlapci))
+print("Zodpovedný za večerný program sú :",random.choice(dievcata),"a",random.choice(chlapci))
